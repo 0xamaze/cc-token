c11f1605cd635b7127d3aa03d9044f6df4df2f36
0xamaze <141855566+0xamaze@users.noreply.github.com>
2023-08-09 19:18:59 +0300

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x5bb64361928bdeecc17dc36bcb72ba050ad783ed";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
